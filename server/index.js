cf8944bfaace20d8ddd96e81427e44db8bddd8bd
KayodeAwe <beekay4laif@yahoo.com>
2024-07-06 12:45:08 +0100

    verifyClient

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const WebSocket = require('ws')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use('/', express.static(path.resolve(__dirname, '../client')))
+
+const server = app.listen(9876)
 
 const wss = new WebSocket.Server({
-    port: 9876
+    server,
+    verifyClient: (info) =>{
+        console.log(info)
+        return true
+    }
 })
 
 wss.on('connection', function(ws){
-    // ws.send('Hello from the server')
     ws.on('message', function(data){
-        // console.log(data.toString())
-        ws.send(data.toString())
+        // ws.send(data.toString())
+        ws.clients.forEach(function each(client){
+            if(client.readyState === WebSocket.OPEN){
+                client.send(data)
+            }
+        })
     })
 })
